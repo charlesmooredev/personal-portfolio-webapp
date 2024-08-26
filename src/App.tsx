15ed66fa41e3da5fec088fa1b016aa85a0c200e1
Charles Moore <charles@ozone.tech>
2024-08-26 12:26:10 -0400

    Add in google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import { RouteManager } from './app/components/RouteManager'
 import 'animate.css/animate.compat.css'
+import { useEffect } from 'react'
+import ReactGA from 'react-ga4'
 
 function App() {
+  const location = window.location
+
+  useEffect(() => {
+    ReactGA.initialize('G-86PK8XB39Y')
+    ReactGA.send({
+      hitType: 'pageview',
+      page: location.pathname,
+      title: location.pathname === '/' ? 'Landing Page' : 'Contact Page',
+    })
+  }, [location.pathname])
+
   return <RouteManager />
 }
 
